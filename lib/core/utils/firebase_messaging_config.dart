1a6a20f2dd9cdd8cd4c0486e28bbebecae9e6a24
mukramm_ <99308248+mmukramm@users.noreply.github.com>
2024-06-20 14:36:55 +0800

    `feat`: handle foreground notification

--- lib/core/utils/firebase_messaging_config.dart
+++ lib/core/utils/firebase_messaging_config.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:unischedule_app/core/utils/credential_saver.dart';
+import 'package:unischedule_app/core/utils/keys.dart';
 import 'package:unischedule_app/features/data/datasources/auth_preferences_helper.dart';
+import 'package:unischedule_app/features/presentation/common/wrapper.dart';
 import 'package:unischedule_app/injection_container.dart';
 
 class FirebaseMessagingConfig {
@@ -23,10 +28,71 @@ class FirebaseMessagingConfig {
       }
     }
 
+    // iOS Foreground Notification
+    await _firebaseMessaging.setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    // For Android Notification in foreground
+    FirebaseMessaging.onMessage.listen(handleForegroundMessage);
+
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+
     debugPrint('Token: $fcmToken');
   }
 
-  void handleMessage(RemoteMessage? message) {
-    if (message == null) return;
+  void handleForegroundMessage(RemoteMessage message) async {
+    final notification = message.notification;
+
+    if (notification == null) return;
+
+    const channel = AndroidNotificationChannel(
+      'high_importance_channel',
+      'High Importance Notifications',
+      description: 'This channel is used for important notifications.',
+      importance: Importance.max,
+    );
+
+    final localNotifications = FlutterLocalNotificationsPlugin();
+
+    await localNotifications.initialize(
+      const InitializationSettings(
+        android: AndroidInitializationSettings('@drawable/ic_launcher'),
+        iOS: DarwinInitializationSettings(),
+      ),
+      onDidReceiveNotificationResponse: (details) {
+        if (CredentialSaver.userInfo != null) {
+          navigatorKey.currentState!.push(
+            MaterialPageRoute(builder: (_) => const Wrapper()),
+          );
+        }
+      },
+    );
+
+    await localNotifications
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+
+    localNotifications.show(
+      notification.hashCode,
+      notification.title,
+      notification.body,
+      NotificationDetails(
+        android: AndroidNotificationDetails(
+          channel.id,
+          channel.name,
+          channelDescription: channel.description,
+          importance: channel.importance,
+          icon: '@drawable/ic_launcher',
+        ),
+        iOS: const DarwinNotificationDetails(),
+      ),
+      payload: jsonEncode(message.toMap()),
+    );
   }
 }
+
+Future<void> handleBackgroundMessage(RemoteMessage message) async {}
